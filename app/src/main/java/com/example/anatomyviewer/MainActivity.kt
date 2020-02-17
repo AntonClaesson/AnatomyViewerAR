027ed41766c549d9019b69f2d621eef57122d814
Anton Claesson <anton.claesson@hotmail.se>
2020-02-17 15:36:09 +0100

    Set up ArFragment

--- app/src/main/java/com/example/anatomyviewer/MainActivity.kt
+++ app/src/main/java/com/example/anatomyviewer/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.anatomyviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
+
+    var arFragment: ArFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+
+
+
     }
 }
