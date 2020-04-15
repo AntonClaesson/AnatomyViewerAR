973a51d214e3a4122f8413c0b526c7c0f26b897c
Anton Claesson <anton.claesson@hotmail.se>
2020-04-15 14:13:42 +0200

    saving

--- app/src/main/java/com/example/anatomyviewer/ARViewModel.kt
+++ app/src/main/java/com/example/anatomyviewer/ARViewModel.kt
@@ -4,14 +4,31 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.google.ar.core.*
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.collision.Box
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.DpToMetersViewSizer
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.TransformableNode
+import com.google.ar.sceneform.ux.TransformationSystem
 import java.io.IOException
 import java.lang.IllegalArgumentException
 
-class ARViewModel: ViewModel() {
+class ARViewModel(): ViewModel() {
 
+
+    //DATA
     val IMAGE_1_NAME: String = "building.jpg"
     val IMAGE_2_NAME: String = "earth.jpg"
     val IMAGE_3_NAME: String = "queen_of_diamonds.jpg"
@@ -22,41 +39,58 @@ class ARViewModel: ViewModel() {
     val trackedImageUpdated = MutableLiveData<Event<AugmentedImage?>>()
 
     // Instance variables
+    var arSceneView: ArSceneView? = null
     var trackNewImages: Boolean = true
     var currentlyTrackedImage: AugmentedImage? = null
         set(value) {
             field = value
             trackedImageUpdated.value = Event(value)
         }
+    private var modelAnchorNode: AnchorNode? = null     // A node attached to the tracked AugmentedImage anchor
+    private var modelNode: TransformableNode? = null    // A node to which the model is attached
+    //private var infoCardNode: Node? = null
+    private var quizCardNode: Node? = null
+    private lateinit var context: Context
+    private lateinit var transformationSystem: TransformationSystem
 
+    fun setup(arSceneView: ArSceneView, context: Context, transformationSystem: TransformationSystem, lifecycleOwner: LifecycleOwner){
+        this.arSceneView = arSceneView
+        this.context = context
+        this.transformationSystem = transformationSystem
 
-    fun reset(){
+        //Setup observers
+        trackedImageUpdated.observe(lifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandledOrReturnNull()?.let { image ->
+                Toast.makeText( context, "Changed to tracking: " + image.name, Toast.LENGTH_LONG).show()
+                createModelForTrackedImage()
+                createQuizRenderable()
+            }
+        })
+    }
+
+    fun reset(context: Context){
         currentlyTrackedImage = null
         trackNewImages = true
-    }
 
-
-    fun setupAugmentedImageDatabase(context: Context, config: Config, session: Session): Boolean {
-
-        fun loadAugmentedImageBitmap(imageName: String): Bitmap = context.assets.open(imageName).use { return BitmapFactory.decodeStream(it) }
-
-        try {
-            config.augmentedImageDatabase = AugmentedImageDatabase(session).also { database ->
-                database.addImage(IMAGE_1_NAME,loadAugmentedImageBitmap(IMAGE_1_NAME))
-                database.addImage(IMAGE_2_NAME,loadAugmentedImageBitmap(IMAGE_2_NAME))
-                database.addImage(IMAGE_3_NAME,loadAugmentedImageBitmap(IMAGE_3_NAME))
-                database.addImage(IMAGE_4_NAME,loadAugmentedImageBitmap(IMAGE_4_NAME))
-
+        if (modelAnchorNode != null) {
+            val anchorNode = modelAnchorNode!!
+            if (anchorNode.anchor != null) {
+                anchorNode.anchor!!.detach()
             }
-            return true
-        } catch (e: IllegalArgumentException) {
-            Log.e(TAG,"Could not add bitmap to augmented image database", e)
-        } catch (e: IOException) {
-            Log.e(TAG, "IO exception loading augmented image bitmap", e)
+            this.arSceneView?.scene?.removeChild(modelAnchorNode)
         }
-        return false
+
+        arSceneView?.scene?.removeChild(quizCardNode)
+        quizCardNode?.setParent(null)
+        quizCardNode = null
+        Toast.makeText(context, "Scanning for new image", Toast.LENGTH_SHORT).show()
     }
 
+    fun onUpdate(frame: Frame) {
+        updateTrackedImageForFrame(frame)
+       // updateCardNodeForFrame(frame, infoCardNode)
+        updateCardNodeForFrame(frame, quizCardNode)
+    }
 
     fun updateTrackedImageForFrame(frame: Frame) {
         // Check if tracking of new images is requested, otherwise return
@@ -85,9 +119,155 @@ class ARViewModel: ViewModel() {
         }
     }
 
+    private fun updateCardNodeForFrame(frame: Frame, node: Node?){
+        val cardNode = node ?: return
 
-    init {
-        Log.i(TAG, "ARViewModel created!")
+        val cameraPosition = Vector3(frame.camera.pose.tx(),frame.camera.pose.ty(),frame.camera.pose.tz())
+        val cardPosition = cardNode.worldPosition
+        val direction = Vector3.subtract(cameraPosition, cardPosition)
+        val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
+        cardNode.worldRotation = lookRotation
+    }
+
+    // Adds the 3D model corresponding to the tracked image to the scene.
+    private fun createModelForTrackedImage() {
+        val trackedImage = currentlyTrackedImage ?: return
+
+        val id = trackedImage.name
+        var model = R.raw.dino //Default fallback model
+        when(id){
+            IMAGE_3_NAME -> {model = R.raw.bone }
+            IMAGE_4_NAME -> {model = R.raw.bone_with_heart_kidney}
+        }
+
+        // Load model from file
+        ModelRenderable.builder().setSource(context, model).build().thenAccept { renderable ->
+            renderable.isShadowCaster = true
+            renderable.isShadowReceiver = false
+
+            val anchor = trackedImage.createAnchor(trackedImage.centerPose)
+            modelAnchorNode = AnchorNode(anchor).apply {
+                setParent(arSceneView?.scene)
+            }
+
+            // Finish loading of model
+            val node = TransformableNode(this.transformationSystem)
+            node.rotationController.isEnabled = true
+            node.scaleController.isEnabled = true
+            node.translationController.isEnabled = false
+            node.scaleController.maxScale =node.scaleController.maxScale*1.0f
+            node.scaleController.minScale = node.scaleController.minScale*0.1f
+
+            node.localPosition = Vector3(0f,0.05f,0f)
+
+
+            modelNode = node
+            node.setParent(modelAnchorNode)
+            node.renderable = renderable
+            node.select()
+
+           // createViewRenderableForNode(node)
+
+        }
+            .exceptionally { throwable ->
+                Log.e(TAG, "Could not create ModelRenderable", throwable)
+                return@exceptionally null
+            }
+    }
+
+    private fun createQuizRenderable() {
+        val id = currentlyTrackedImage?.name ?: return
+        var currentQuiz=1
+        when (id) {
+            IMAGE_3_NAME -> {currentQuiz=3}
+            IMAGE_4_NAME -> {currentQuiz=4}
+        }
+
+        val quizNode = Node()
+        quizNode.setParent(arSceneView?.scene)
+
+        val dpm = 500 //Default 250 dpm
+        ViewRenderable.builder().setView(context, R.layout.quiz_card).build()
+            .thenAccept { viewRenderable ->
+                viewRenderable.setSizer { DpToMetersViewSizer(dpm).getSize(viewRenderable.view) }
+                viewRenderable.isShadowCaster = false
+                viewRenderable.isShadowReceiver = false
+                quizNode.renderable = viewRenderable
+                quizCardNode = quizNode
+            }
+    }
+
+    private fun createViewRenderableForNode(parent: Node) {
+        val id = currentlyTrackedImage?.name ?: return
+        var title = "Not available"
+        var description = "Not available"
+        when (id) {
+            IMAGE_3_NAME -> {
+                title = "Skeleton"; description = "This is the skeleton of a human torso"
+            }
+            IMAGE_4_NAME -> {
+                title = "Torso"; description =
+                    "This is the skeleton, heart and kidneys of a human torso"
+            }
+        }
+
+        val dpm = 500 //Default 250 dpm
+        ViewRenderable.builder().setView(context, R.layout.info_card).build()
+            .thenAccept { viewRenderable ->
+                viewRenderable.setSizer { DpToMetersViewSizer(dpm).getSize(viewRenderable.view) }
+                val titleTextView = viewRenderable.view.findViewById<TextView>(R.id.title)
+                titleTextView.text = title
+                val descriptionTextView =
+                    viewRenderable.view.findViewById<TextView>(R.id.description)
+                descriptionTextView.text = description
+
+                viewRenderable.isShadowCaster = false
+                viewRenderable.isShadowReceiver = false
+
+                val infoCardNode = Node()
+                infoCardNode.setParent(parent)
+                infoCardNode.renderable = viewRenderable
+
+                val modelBoundingBox = parent.collisionShape as Box
+                val infoCardBoundingBox = infoCardNode.collisionShape as Box
+
+                infoCardNode.localPosition = Vector3(
+                    modelBoundingBox.extents.x / 2 + infoCardBoundingBox.extents.x / 2 + 0.05f,
+                    0.2f,
+                    0f
+                )
+
+                infoCardNode.setOnTapListener { hitTestResult, motionEvent ->
+                    Toast.makeText(
+                        this.context,
+                        "Tapped infoCard card!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+              //  this.infoCardNode = infoCardNode
+            }
+    }
+
+    fun setupAugmentedImageDatabase(context: Context, config: Config, session: Session): Boolean {
+
+        fun loadAugmentedImageBitmap(imageName: String): Bitmap = context.assets.open(imageName).use { return BitmapFactory.decodeStream(it) }
+
+        try {
+            config.augmentedImageDatabase = AugmentedImageDatabase(session).also { database ->
+                database.addImage(IMAGE_1_NAME,loadAugmentedImageBitmap(IMAGE_1_NAME))
+                database.addImage(IMAGE_2_NAME,loadAugmentedImageBitmap(IMAGE_2_NAME))
+                database.addImage(IMAGE_3_NAME,loadAugmentedImageBitmap(IMAGE_3_NAME))
+                database.addImage(IMAGE_4_NAME,loadAugmentedImageBitmap(IMAGE_4_NAME))
+
+            }
+            return true
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG,"Could not add bitmap to augmented image database", e)
+        } catch (e: IOException) {
+            Log.e(TAG, "IO exception loading augmented image bitmap", e)
+        }
+        return false
     }
 
     override fun onCleared() {
--- app/src/main/java/com/example/anatomyviewer/AnatomyViewerFragment.kt
+++ app/src/main/java/com/example/anatomyviewer/AnatomyViewerFragment.kt
@@ -1,5 +1,8 @@
 package com.example.anatomyviewer
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -21,40 +24,29 @@ import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import com.google.ar.sceneform.math.Quaternion
-
+import java.io.IOException
+import java.lang.IllegalArgumentException
 
 
 open class AnatomyViewerFragment : ArFragment() {
 
+    private val TAG = AnatomyViewerFragment::class.java.toString()
+
     // ViewModel containing data and business logic
     private lateinit var viewModel: ARViewModel
-
-    private var modelAnchorNode: AnchorNode? = null     // A node attached to the tracked AugmentedImage anchor
-    private var modelNode: TransformableNode? = null    // A node to which the model is attached
-    private var infoCardNode: Node? = null
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view =  super.onCreateView(inflater, container, savedInstanceState)
 
         //Instantiate ViewModel
         viewModel = ViewModelProvider(this).get(ARViewModel::class.java)
-
+        viewModel.setup(arSceneView,requireContext(),transformationSystem,viewLifecycleOwner)
         //Hides the scan floor gif
         this.planeDiscoveryController.hide()
         this.planeDiscoveryController.setInstructionView(null)
 
         //Hides the plane renderering
         this.arSceneView.planeRenderer.isVisible = false
-
-        //Setup observers
-        viewModel.trackedImageUpdated.observe(viewLifecycleOwner, Observer { event ->
-            event?.getContentIfNotHandledOrReturnNull()?.let { image ->
-                Toast.makeText( this.requireContext(), "Changed to tracking: " + image.name, Toast.LENGTH_LONG).show()
-                createModelForTrackedImage()
-            }
-        })
-
         return view
     }
 
@@ -74,171 +66,13 @@ open class AnatomyViewerFragment : ArFragment() {
     }
 
     fun resetSession(){
-        viewModel.reset()
-
-        if (modelAnchorNode != null) {
-            val anchorNode = modelAnchorNode!!
-            if (anchorNode.anchor != null) {
-                anchorNode.anchor!!.detach()
-            }
-            this.arSceneView.scene.removeChild(modelAnchorNode)
-        }
-
-        Toast.makeText(this.requireContext(), "Scanning for new image", Toast.LENGTH_SHORT).show()
+        viewModel.reset(this.requireContext())
     }
 
     override fun onUpdate(frameTime: FrameTime?) {
         super.onUpdate(frameTime)
         val frame = this.arSceneView.arFrame ?: return
-        // If tracking is ok, we proceed
-        if (trackingStateOK(frame)) {
-            viewModel.updateTrackedImageForFrame(frame)
-            updateinfoCardNodeForFrame(frame)
-        } else {
-            handleBadTracking()
-        }
-    }
-
-
-    // Checks whether tracking is active or not.
-    private fun trackingStateOK(frame: Frame): Boolean {
-        if (frame.camera.trackingState == TrackingState.TRACKING) return true
-        return false
-    }
-
-    private fun handleBadTracking() {
-        //TODO: Handle bad tracking
-    }
-
-    private fun updateinfoCardNodeForFrame(frame: Frame){
-        val infoCardNode = this.infoCardNode ?: return
-
-        val cameraPosition = Vector3(frame.camera.pose.tx(),frame.camera.pose.ty(),frame.camera.pose.tz())
-        val cardPosition = infoCardNode.worldPosition
-        val direction = Vector3.subtract(cameraPosition, cardPosition)
-        val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
-        infoCardNode.worldRotation = lookRotation
-    }
-
-    // Adds the 3D model corresponding to the tracked image to the scene.
-    private fun createModelForTrackedImage() {
-        val trackedImage = viewModel.currentlyTrackedImage ?: return
-
-        val id = trackedImage.name
-        var model = R.raw.dino //Default fallback model
-        when(id){
-            viewModel.IMAGE_3_NAME -> {model = R.raw.bone }
-            viewModel.IMAGE_4_NAME -> {model = R.raw.bone_with_heart_kidney}
-        }
-
-        // Load model from file
-        ModelRenderable.builder().setSource(this.requireContext(), model).build().thenAccept { renderable ->
-            renderable.isShadowCaster = true
-            renderable.isShadowReceiver = false
-
-            val anchor = trackedImage.createAnchor(trackedImage.centerPose)
-            modelAnchorNode = AnchorNode(anchor).apply {
-                setParent(this@AnatomyViewerFragment.arSceneView.scene)
-            }
-
-
-            // Finish loading of model
-            val node = TransformableNode(this.transformationSystem)
-            node.rotationController.isEnabled = true
-            node.scaleController.isEnabled = true
-            node.translationController.isEnabled = false
-            node.scaleController.maxScale =node.scaleController.maxScale*1.0f
-            node.scaleController.minScale = node.scaleController.minScale*0.1f
-
-            node.localPosition = Vector3(0f,0.05f,0f)
-
-
-            modelNode = node
-            node.setParent(modelAnchorNode)
-            node.renderable = renderable
-            node.select()
-
-            createViewRenderableForNode(node)
-
-        }
-            .exceptionally { throwable ->
-                Log.e(TAG, "Could not create ModelRenderable", throwable)
-                return@exceptionally null
-            }
-    }
-
-    private fun createViewRenderableForNode(parent: Node){
-
-        val id = viewModel.currentlyTrackedImage?.name  ?: return
-
-        var title = "Not available"
-        var description = "Not available"
-
-        when (id) {
-            viewModel.IMAGE_3_NAME -> { title = "Skeleton"; description = "This is the skeleton of a human torso"}
-            viewModel.IMAGE_4_NAME -> { title = "Torso"; description = "This is the skeleton, heart and kidneys of a human torso"}
-        }
-
-        val dpm = 500 //Default 250 dpm
-
-        ViewRenderable.builder().setView(requireContext(), R.layout.info_card).build().thenAccept { viewRenderable ->
-
-            viewRenderable.setSizer { DpToMetersViewSizer(dpm).getSize(viewRenderable.view) }
-            val titleTextView = viewRenderable.view.findViewById<TextView>(R.id.title)
-            titleTextView.text = title
-            val descriptionTextView = viewRenderable.view.findViewById<TextView>(R.id.description)
-            descriptionTextView.text = description
-
-            viewRenderable.isShadowCaster = false
-            viewRenderable.isShadowReceiver = false
-            
-            val infoCardNode = Node()
-            infoCardNode.setParent(parent)
-            infoCardNode.renderable = viewRenderable
-
-            val modelBoundingBox = parent.collisionShape as Box
-            val infoCardBoundingBox = infoCardNode.collisionShape as Box
-
-            infoCardNode.localPosition = Vector3(
-                modelBoundingBox.extents.x/2 + infoCardBoundingBox.extents.x/2 + 0.05f,
-                0.2f,
-                0f)
-
-            infoCardNode.setOnTapListener { hitTestResult, motionEvent ->
-                Toast.makeText( this.requireContext(), "Tapped infoCard card!", Toast.LENGTH_LONG).show()
-            }
-
-            this.infoCardNode = infoCardNode
-        }
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    companion object {
-        private val TAG: String = AnatomyViewerFragment::class.java.simpleName
+        viewModel.onUpdate(frame)
     }
 
 }
